0736ff0e4e728a0c3e97551a124599851f88a858
shanME <shanME@toolsQA.com>
2020-07-04 15:21:29 +0530

    Development of Feature B complete

--- src/com/toolsqa/gitcourse/HelloWorld.java
+++ src/com/toolsqa/gitcourse/HelloWorld.java
@@ -3,8 +3,9 @@ package com.toolsqa.gitcourse;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("HelloWorld!");
+		
+		System.out.println("Developed Feature B!");
 	}
 
 }
